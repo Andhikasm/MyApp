dd3281c91a213e59c28b53ff14d8ff6669f51c71
Andhika Srimadeva <andhika.srimadeva@kcl.ac.uk>
2017-08-16 12:57:04 +0100

    Fix error with navigation bar profile pic

--- app/src/main/java/com/andhikasrimadeva/myapp/Users.java
+++ app/src/main/java/com/andhikasrimadeva/myapp/Users.java
@@ -5,4 +5,26 @@ package com.andhikasrimadeva.myapp;
  */
 
 public class Users {
+
+    private String name;
+    private String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+
 }
--- app/src/main/java/com/andhikasrimadeva/myapp/UsersActivity.java
+++ app/src/main/java/com/andhikasrimadeva/myapp/UsersActivity.java
@@ -2,18 +2,62 @@ package com.andhikasrimadeva.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UsersActivity extends AppCompatActivity {
 
     private RecyclerView mUsersList;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
         mUsersList = (RecyclerView) findViewById(R.id.users_list);
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
         setTitle("All Users");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mUsersList.setHasFixedSize(true);
+        mUsersList.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Users, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>(
+                Users.class,
+                R.layout.single_user_layout,
+                UsersViewHolder.class,
+                mDatabaseReference
+        ) {
+            @Override
+            protected void populateViewHolder(UsersViewHolder viewHolder, Users model, int position) {
+                viewHolder.setName(model.getName());
+            }
+        };
+        mUsersList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class UsersViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setName(String name) {
+            TextView userNameView = (TextView) mView.findViewById(R.id.user_single_name);
+            userNameView.setText(name);
+        }
     }
 }
