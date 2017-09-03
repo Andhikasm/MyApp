fe0941687175b26a195959440c373705961f43da
Andhika Srimadeva <andhika.srimadeva@kcl.ac.uk>
2017-09-03 12:56:15 +0700

    Set the references for fields in FriendsFragment

--- app/src/main/java/com/andhikasrimadeva/myapp/FriendsFragment.java
+++ app/src/main/java/com/andhikasrimadeva/myapp/FriendsFragment.java
@@ -3,9 +3,15 @@ package com.andhikasrimadeva.myapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
@@ -13,6 +19,11 @@ import android.view.ViewGroup;
  */
 public class FriendsFragment extends Fragment {
 
+    private RecyclerView friendsList;
+    private View mainView;
+
+    private DatabaseReference friendsDatabase;
+    private String current_user_id;
 
     public FriendsFragment() {
         // Required empty public constructor
@@ -20,10 +31,25 @@ public class FriendsFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        mainView = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        friendsList = (RecyclerView) mainView.findViewById(R.id.friends_list);
+        current_user_id = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        friendsDatabase = FirebaseDatabase.getInstance().getReference().child("Friends").child(current_user_id);
+
+        friendsList.setHasFixedSize(true);
+        friendsList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return mainView;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+    }
 }
