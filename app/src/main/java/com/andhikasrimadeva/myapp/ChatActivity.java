722dce1b8ee3fa31336c9a92aed96732f675b680
Andhika Srimadeva <andhika.srimadeva@kcl.ac.uk>
2017-09-05 00:55:42 +0700

    Implement functionality to display received messages from db

--- app/src/main/java/com/andhikasrimadeva/myapp/ChatActivity.java
+++ app/src/main/java/com/andhikasrimadeva/myapp/ChatActivity.java
@@ -1,9 +1,11 @@
 package com.andhikasrimadeva.myapp;
 
 import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
@@ -15,14 +17,18 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -43,8 +49,16 @@ public class ChatActivity extends AppCompatActivity {
     private ImageButton mChatAddBtn;
     private ImageButton mChatSendBtn;
     private EditText mChatMessageView;
+    private SwipeRefreshLayout mRefreshLayout;
 
     private RecyclerView mMessagesList;
+
+    private final List<Messages> messagesList = new ArrayList<>();
+    private LinearLayoutManager mLinearLayout;
+    private MessageAdapter mAdapter;
+
+    private static final int TOTAL_ITEMS_TO_LOAD = 10;
+    private int mCurrentPage = 1;
 
 
     @Override
@@ -78,6 +92,19 @@ public class ChatActivity extends AppCompatActivity {
         mChatAddBtn = (ImageButton) findViewById(R.id.chat_add_btn);
         mChatSendBtn = (ImageButton) findViewById(R.id.chat_send_btn);
         mChatMessageView = (EditText) findViewById(R.id.chat_message_view);
+        mRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.message_swipe_layout);
+
+        mAdapter = new MessageAdapter(messagesList);
+
+        mMessagesList = (RecyclerView) findViewById(R.id.messages_list);
+        mLinearLayout = new LinearLayoutManager(this);
+
+        mMessagesList.setHasFixedSize(true);
+        mMessagesList.setLayoutManager(mLinearLayout);
+
+        mMessagesList.setAdapter(mAdapter);
+
+        loadMessages();
 
         mTitleView.setText(userName);
 
@@ -154,6 +181,17 @@ public class ChatActivity extends AppCompatActivity {
 
             }
         });
+
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mCurrentPage++;
+
+                messagesList.clear();
+
+                loadMessages();
+            }
+        });
     }
 
     private void sendMessage() {
@@ -198,6 +236,50 @@ public class ChatActivity extends AppCompatActivity {
             });
 
         }
+
+    }
+
+    private void loadMessages() {
+
+        DatabaseReference messageRef = mRootRef.child("Messages").child(mCurrentUserId).child(mChatUser);
+
+        Query messageQuery = messageRef.limitToLast(mCurrentPage * TOTAL_ITEMS_TO_LOAD);
+
+        messageQuery.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                Messages message = dataSnapshot.getValue(Messages.class);
+
+                messagesList.add(message);
+                mAdapter.notifyDataSetChanged();
+
+                mMessagesList.scrollToPosition(messagesList.size() - 1);
+
+                mRefreshLayout.setRefreshing(false);
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
